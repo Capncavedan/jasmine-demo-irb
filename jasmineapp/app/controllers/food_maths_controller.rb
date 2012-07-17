493520f37a18d210542679c2797f1caf1330d66f
Dan Buettner <dbuettner@merchantsbonding.com>
2012-07-17 13:36:27 -0500

    add CSS, and page with select box for flavor

--- jasmineapp/app/controllers/food_maths_controller.rb
+++ jasmineapp/app/controllers/food_maths_controller.rb
@@ -1,6 +1,7 @@
 class FoodMathsController < ApplicationController
 
   def index
+    @flavors = ['Raspberry', 'Lemon Merengue', 'Rhubarb', 'Mincemear', 'Dutch Apple a la mode', 'I mean it']
   end
 
 end

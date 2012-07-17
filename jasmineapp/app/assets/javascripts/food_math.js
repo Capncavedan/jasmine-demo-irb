5f54524555cee814fbc2596f8f9a7a459906d4af
Dan Buettner <dbuettner@merchantsbonding.com>
2012-07-17 14:52:36 -0500

    add working DOM spec and fixtures

--- jasmineapp/app/assets/javascripts/food_math.js
+++ jasmineapp/app/assets/javascripts/food_math.js
@@ -1,7 +1,26 @@
 var FoodMath = {
 
-  pi: function() {
+  pi: function(flavor) {
+    if (flavor == 'Dutch Apple a la mode')
+      return "is the very best!";
+
+    if (!flavor || /^\s*$/.test(flavor))
+      return '';
+
     return "is yummy!";
   }
 
 };
+
+(function($){
+  $.fn.flavorInfoSetter = function() {
+    $(this).change(function() {
+      var newInfo = FoodMath.pi( $('#flavor_select').val() );
+      $('#flavor_info').html(newInfo);
+    });
+  };
+})(jQuery);
+
+$(function() {
+  $("#flavor_select").flavorInfoSetter();
+});
